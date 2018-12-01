5922f79ce5bb6ebddab3ff119339390ceb524fb0
Philip Loa <boyet007@gmail.com>
2018-12-01 14:42:25 +0700

    signup - fix bug (not finished)

--- src/components/User/Signup.vue
+++ src/components/User/Signup.vue
@@ -5,51 +5,70 @@
                 <app-alert @dismissed="onDismissed" :text="error.message"></app-alert>
             </v-flex>
         </v-layout>
-        <v-layout>
+        <v-layout row>
             <v-flex xs12 sm6 offset-sm3>
-                <v-card-text>
-                    <v-container>
-                        <form @submit.prevent="onSignup">
-                            <v-layout row>
-                                <v-flex xs12>
-                                    <v-text-field name="email" label="Mail" 
-                                    id="email" v-model="email" type="email"
-                                    required></v-text-field>
-                                </v-flex>
-                            </v-layout>
-                            <v-layout row>
-                                <v-flex xs12>
-                                    <v-text-field name="password" label="Password" 
-                                    id="password" v-model="password" type="password"
-                                    required></v-text-field>
-                                </v-flex>
-                            </v-layout>
-                            <v-layout row>
-                                <v-flex xs12>
-                                    <v-text-field name="confirmPassword" label="Confirm Password" 
-                                    id="confirmPassword" v-model="confirmPassword" type="password"
-                                    :rules="[comparePasswords]
-                                    " required></v-text-field>
-                                </v-flex>
-                            </v-layout>
-                            <v-layout row>
-                                <v-flex xs12>
-                                    <v-btn type="submit" :disabled="loading" :loading="loading">
-                                    Sign Up
-                                    <span slot="loader" class="custom-loader">
-                                        <v-icon light>cached
-                                        </v-icon></span>
-                                    </v-btn>
-                                </v-flex>
-                            </v-layout>
-                        </form>
-                    </v-container>
-                </v-card-text>
+                <v-card>
+                    <v-card-text>
+                        <v-container>
+                            <form @submit.prevent="onSignup">
+                                <v-layout row>
+                                    <v-flex xs12>
+                                        <v-text-field 
+                                            name="email" 
+                                            label="Mail" 
+                                            id="email" 
+                                            v-model="email" 
+                                            type="email"
+                                            required></v-text-field>
+                                    </v-flex>
+                                </v-layout>
+                                <v-layout row>
+                                    <v-flex xs12>
+                                        <v-text-field 
+                                            name="password" 
+                                            label="Password" 
+                                            id="password" 
+                                            v-model="password" 
+                                            type="password"
+                                            required></v-text-field>
+                                    </v-flex>
+                                </v-layout>
+                                <v-layout row>
+                                    <v-flex xs12>
+                                        <v-text-field 
+                                            name="confirmPassword" 
+                                            label="Confirm Password" 
+                                            id="confirmPassword" 
+                                            v-model="confirmPassword" 
+                                            type="password"
+                                            :rules="[comparePasswords]"
+                                            required></v-text-field>
+                                    </v-flex>
+                                </v-layout>
+                                <v-layout row>
+                                    <v-flex xs12>
+                                        <v-btn 
+                                            type="submit" 
+                                            :disabled="loading" 
+                                            :loading="loading">
+                                            Sign Up
+                                        <span 
+                                            slot="loader" 
+                                            class="custom-loader">
+                                            <v-icon light>cached
+                                            </v-icon>
+                                        </span>
+                                        </v-btn>
+                                    </v-flex>
+                                </v-layout>
+                            </form>
+                        </v-container>
+                    </v-card-text>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
 </template>
-
 <script>
 export default {
     data() {
@@ -57,7 +76,6 @@ export default {
             email: 'siska@yahoo.com',
             password: 'Wynne321',
             confirmPassword: 'Wynne321',
-            loading: false
         }
     },
     computed: {
@@ -83,57 +101,12 @@ export default {
     },
     methods: {
         onSignup() { 
-        //    console.log({email: this.email, password: this.password, 
-        //     confirmPassword: this.confirmPassword});
-            console.log(this.email)
-            console.log(this.password)
             this.$store.dispatch('signUserUp', {email : this.email, password: this.password})
             
         },
         onDismissed() {
-            console.log('Dismissed Alert!')
             this.$store.dispatch('clearError')
         }
     }
 }
 </script>
-
-<style>
-  .custom-loader {
-    animation: loader 1s infinite;
-    display: flex;
-  }
-  @-moz-keyframes loader {
-    from {
-      transform: rotate(0);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
-  @-webkit-keyframes loader {
-    from {
-      transform: rotate(0);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
-  @-o-keyframes loader {
-    from {
-      transform: rotate(0);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
-  @keyframes loader {
-    from {
-      transform: rotate(0);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
-</style>
-
